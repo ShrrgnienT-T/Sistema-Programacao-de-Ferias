a0f68b7503ca95d584593727f68c79ecbe8e221a
Eduardo Lima Oliveira <107649155+ShrrgnienT-T@users.noreply.github.com>
2026-02-25 13:54:48 -0400

    Replace AdminLTE with Morhena default layout and split dashboard UI into Blade/JS components

---
+++ resources/views/dashboard/partials/tab-cadastro.blade.php
@@ -0,0 +1,6 @@
+<div class="tab" id="tab-cadastro">
+    <div class="table-card p-4">
+        <p class="section-sub mb-2">Cadastro transacional oficial do sistema.</p>
+        <a href="{{ route('employees.index') }}" class="btn btn-primary">Ir para Colaboradores</a>
+    </div>
+</div>
